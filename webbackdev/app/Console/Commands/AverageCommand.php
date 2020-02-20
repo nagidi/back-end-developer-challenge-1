84c99999cdff0c55b083dbd03dc6b7b4873c5f98
srinivas <srinivas.nagidi@gmail.com>
2020-02-20 11:40:06 -0500

    Performance issue fix done.. Thanks

--- webbackdev/app/Console/Commands/AverageCommand.php
+++ webbackdev/app/Console/Commands/AverageCommand.php
@@ -40,12 +40,9 @@ class AverageCommand extends Command
      */
     public function handle()
     {		
-		$states = State::orderBy('state')->get();		
-		foreach($states as $state){			
-			if(count($state->copis) > 0){
-				$copidetails = Copi::selectRaw('round(AVG(steak),0) AS steak')->selectRaw('round(AVG(grnd_beef),0) as beef')->selectRaw('round(AVG(sausage),0) as sausage')->selectRaw('round(AVG(fry_chick),0) as frychick')->selectRaw('round(AVG(tuna),0) as tuna')->where('state', '=', $state->state)->groupBy('state')->first();
-				echo sprintf("%s : Steak (%d), Ground Beef (%d), Sausage (%d), Fried Chicken (%d), Tuna (%d)",$state->state,$copidetails->steak,$copidetails->beef,$copidetails->sausage,$copidetails->frychick,$copidetails->tuna).PHP_EOL;
-			}
+		$copidetails = Copi::selectRaw('AVG(steak) AS steak')->selectRaw('AVG(grnd_beef) as beef')->selectRaw('AVG(sausage) as sausage')->selectRaw('AVG(fry_chick) as frychick')->selectRaw('AVG(tuna) as tuna')->selectRaw('state')->groupBy('state')->get();		
+		foreach($copidetails as $copidetail){
+			echo sprintf("%s : Steak (%d), Ground Beef (%d), Sausage (%d), Fried Chicken (%d), Tuna (%d)",$copidetail->state,$copidetail->steak,$copidetail->beef,$copidetail->sausage,$copidetail->frychick,$copidetail->tuna).PHP_EOL;
 		}
     }
 }
